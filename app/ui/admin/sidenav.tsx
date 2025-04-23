276d60404b5c70bf37bebdcadafe14ae36923c4e
Gabriel Arden Juansava <92002633+gabrielarden@users.noreply.github.com>
2025-04-23 10:43:57 +0700

    Update sidenav.tsx

--- app/ui/admin/sidenav.tsx
+++ app/ui/admin/sidenav.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import AdminSideNav from '@/app/ui/admin/sidenav';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
-        <AdminSideNav />
+        {/* Berikan children jika AdminSideNav memerlukannya */}
+        <AdminSideNav>{children}</AdminSideNav>
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
